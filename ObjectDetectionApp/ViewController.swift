625c18b84212edacf62159b5bee11dd167111252
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-27 22:14:29 +0600

    Initial Commit

---
+++ ObjectDetectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectDetectionApp
+//
+//  Created by Khayrul on 3/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
